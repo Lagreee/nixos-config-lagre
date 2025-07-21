d0999b8051d778f36106b17491f4ef84b0a27324
Lagreee <lagreca.gabriele@gmail.com>
2025-07-21 22:43:33 +0200

    Update tuigreet theme colors in display manager
    Simplified and adjusted the color scheme for the tuigreet command in the display manager module, changing several theme color values to use basic colors for improved clarity and consistency.

--- nixos/modules/display-manager.nix
+++ nixos/modules/display-manager.nix
@@ -13,7 +13,7 @@ in {
     enable = true;
     settings = {
       default_session = {
-        command = "${tuigreet} --time --greeting 'Synchronization ratio: 100% - Welcome back' --theme border=bright_red;text=bright_white;time=bright_cyan;container=black;title=bright_red;greet=bright_white;prompt=bright_red;input=bright_red;action=bright_magenta;button=bright_white --remember --remember-session --sessions ${hyprland-session}";
+        command = "${tuigreet} --time --greeting 'Synchronization ratio: 100% - Welcome back' --theme border=red;prompt=red;text=white;container=black;input=red;time=cyan --remember --remember-session --sessions ${hyprland-session}";
         user = "greeter";
       };
     };
