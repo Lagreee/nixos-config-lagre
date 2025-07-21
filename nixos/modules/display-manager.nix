2252e14496f6c88ad5b31f32efce9d0b1b74dbf5
Lagreee <lagreca.gabriele@gmail.com>
2025-07-21 22:28:45 +0200

    Remove initial_session from greetd settings
    Deleted the initial_session configuration block from services.greetd, leaving only the default_session. This simplifies the display manager setup and removes the hardcoded Hyprland session for user 'lagre'.

--- nixos/modules/display-manager.nix
+++ nixos/modules/display-manager.nix
@@ -12,12 +12,6 @@ in {
   services.greetd = {
     enable = true;
     settings = {
-
-      initial_session = {
-        command = "Hyprland";
-        user = "lagre";
-      };
-
       default_session = {
         command = "${tuigreet} --time --greeting 'Synchronization ratio: 100% - Welcome back' --theme border=red;text=white;prompt=red;time=yellow;action=green;button=bright_yellow;container=black;input=red --remember --remember-session --sessions ${hyprland-session}";
         user = "greeter";
