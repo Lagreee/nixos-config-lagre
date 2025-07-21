e9eac1bbf2b07537e8ad13db29c1a9461cad3331
Lagreee <lagreca.gabriele@gmail.com>
2025-07-21 22:18:29 +0200

    Refactor greetd configuration and update tuigreet options
    Reorganized the display-manager.nix module for improved readability and moved SDDM configuration comments to the end. Updated tuigreet command in greetd's default_session to include a custom greeting and theme options.

--- nixos/modules/display-manager.nix
+++ nixos/modules/display-manager.nix
@@ -1,7 +1,40 @@
-{ pkgs, user, config,  ... }:  let
+{ 
+  pkgs, 
+  user, 
+  config, 
+  ... 
+}:  let
   tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
   hyprland-session = "${pkgs.hyprland}/share/wayland-sessions";
-in{
+in {
+  # ----- Configurazione Greetd + tuigreet ----------
+
+  services.greetd = {
+    enable = true;
+    settings = {
+
+      initial_session = {
+        command = "Hyprland";
+        user = "lagre";
+      };
+
+      default_session = {
+        command = "${tuigreet} --time --greeting 'Synchronization ratio: 100% - Welcome back' --theme border=red;text=white;prompt=red;time=yellow;action=green;button=orange;container=black;input=red --remember --remember-session --sessions ${hyprland-session}";
+        user = "greeter";
+      };
+    };
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
   
   # ----- Configurazione SDDM -----   
 
@@ -19,57 +52,4 @@ in{
   #};
   # ------------------------------
 
-
-  # ----- Configurazione Greetd + tuigreet ----------
-  
-  #services.greetd.enable = true;
-  
-  #services.greetd.settings = {
-  #  initial_session = {
-  #    command = "Hyprland";
-  #    user = "lagre";
-  #  };
-  # 
-  #  default_session = {
-  #    command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --user-menu";
-  #    user = "greeter";
-  #  };
-  #};
-
-  #environment.systemPackages = config.environment.systemPackages ++ [
-  #  pkgs.tuigreet
-  #];
-
-  #users.users.greeter = {
-  #  isSystemUser = true;
-  #  group = "greeter";
-  #};
-
-  #users.groups.greeter = {};
-  # ------------------------------
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
-        user = "greeter";
-      };
-    };
-  };
-
-  # this is a life saver.
-  # literally no documentation about this anywhere.
-  # might be good to write about this...
-  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
-  };
 }
